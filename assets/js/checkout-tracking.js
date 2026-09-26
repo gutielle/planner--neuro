4202fd056c92450f24ffa7c78eae83848e31639f
Gutielle <zgutielle@gmail.com>
2026-09-25 23:47:20 -0300

    Novo preço 37,00

--- assets/js/checkout-tracking.js
+++ assets/js/checkout-tracking.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  var CHECKOUT_BASE = "https://pay.hotmart.com/N107723281D?checkoutMode=10";
+  var CHECKOUT_BASE = "https://pay.kirvano.com/fbc0e348-f821-4962-bfac-222caab581e3";
   var ALLOWED_KEYS = new Set([
     "utm_source", "utm_medium", "utm_campaign", "utm_term", "utm_content", "utm_id",
     "gclid", "fbclid", "wbraid", "gbraid", "msclkid", "src", "sck"
@@ -95,12 +95,12 @@
   function fireTrackingEvents() {
     try {
       if (typeof window.fbq === "function") {
-        window.fbq("track", "InitiateCheckout", { value: 47, currency: "BRL" });
+        window.fbq("track", "InitiateCheckout", { value: 37, currency: "BRL" });
       }
     } catch (_) {}
     try {
       if (typeof window.hot === "function") {
-        window.hot("event", "InitiateCheckout", { value: 47, currency: "BRL" });
+        window.hot("event", "InitiateCheckout", { value: 37, currency: "BRL" });
       }
     } catch (_) {}
   }
